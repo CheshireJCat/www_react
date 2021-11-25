0298552a9bf956604d98dca03687d58b38866428
jinye <lmaomaoz@live.com>
2021-11-25 22:15:11 +0800

    修改分类

--- src/config/category.ts
+++ src/config/category.ts
@@ -2,8 +2,8 @@ const categories = new Map([
   [0, "未分类"],
   [1, "前端"],
   [2, "后端"],
-  [2, "error"],
-  [3, "随笔"],
-  [4, "心情"],
+  [3, "error"],
+  [4, "随笔"],
+  [5, "心情"],
 ]);
 export default categories;
